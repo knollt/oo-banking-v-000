6b3ddd0a1bde1efe3241e21f8894f798e0767407
Tim Knoll <timothy.knoll@gmail.com>
2018-10-18 02:59:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,8 @@ class Transfer
 
   def execute_transaction
     if @sender.balance > @amount && @status == 'pending'
-      
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = 'complete'
   end
 end
