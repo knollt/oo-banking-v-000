d446f3137b3838a12dcf232ffc3eebe820aacd99
Tim Knoll <timothy.knoll@gmail.com>
2018-10-18 02:42:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,10 @@
 class Transfer
-  attr_accessor :sender
+  attr_accessor :sender, :receiver
 
 
   def initialize (sender, receiver, amount)
     @sender = sender
+    @receiver = receiver
   end
 
 end
