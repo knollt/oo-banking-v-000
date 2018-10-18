7f03c7948bb92c4714f0f97d87ec92eb70da1f60
Tim Knoll <timothy.knoll@gmail.com>
2018-10-18 02:29:53 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
+# require 'pry'
+
 class BankAccount
+
+  attr_accessor :balance, :status
+  attr_reader :name
+
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(balance)
+    @balance + balance
+    balance
+    # binding.pry
+  end
 
 end
