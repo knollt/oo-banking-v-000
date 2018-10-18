8f50d30ee1c5c7d2a31319681185c432761fa052
Tim Knoll <timothy.knoll@gmail.com>
2018-10-18 03:06:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,10 @@ class Transfer
   end
 
   def reverse_transfer
-    
-  end   
+    if @status == 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = 'reversed'
+    end   
+  end
 end
