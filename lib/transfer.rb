b93d990c8258839a57375d35fc2c4d1c1853df94
Tim Knoll <timothy.knoll@gmail.com>
2018-10-18 03:02:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,9 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = 'complete'
-    end
+    else
       @status = 'rejected'
       "Transaction rejected. Please check your account balance."
+    end   
   end
 end
