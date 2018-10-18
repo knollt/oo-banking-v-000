89b8860cb069e507900794a15f8a9aa7f333972a
Tim Knoll <timothy.knoll@gmail.com>
2018-10-18 02:52:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,4 +13,8 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end
 
+  def execute_transaction
+    if @sender.balance > @amount && @status == 'pending'
+      
+  end
 end
